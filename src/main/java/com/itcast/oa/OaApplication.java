642f7a0b09995bc338922b3c7c52de227a668764
liang7962 <liang4032>
2018-11-25 09:21:19 +0800

    commit two

--- src/main/java/com/itcast/oa/OaApplication.java
+++ src/main/java/com/itcast/oa/OaApplication.java
@@ -2,8 +2,17 @@ package com.itcast.oa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+
+@ServletComponentScan
 @SpringBootApplication
+@PropertySource({"classpath:application.properties"})
+@ImportResource({"classpath:/spring/applicationContext*.xml"})
+@EnableScheduling
 public class OaApplication {
 
 	public static void main(String[] args) {
